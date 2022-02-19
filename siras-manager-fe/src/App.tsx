faf254adffa7a6183dbc3b71a32f67554aa78e10
Tom Huang <tom6311tom6311@gmail.com>
2022-02-19 18:27:41 +0800

    feat: integrate redux toolkit and apply to SiteListPage
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- siras-manager-fe/src/App.tsx
+++ siras-manager-fe/src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import {
   BrowserRouter, Routes, Route,
 } from 'react-router-dom';
+import store from './store';
 import Header from './components/Header/Header';
-import OverviewListPage from './containers/OverviewListPage/OverviewListPage';
+import SiteListPage from './containers/SiteListPage/SiteListPage';
 
 const App = () => (
-  <BrowserRouter>
-    <Header title="魚電養殖管理" />
-    <Routes>
-      <Route path="*" element={<OverviewListPage />} />
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header title="魚電養殖管理" />
+      <Routes>
+        <Route path="*" element={<SiteListPage />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 )
 
 export default App;
