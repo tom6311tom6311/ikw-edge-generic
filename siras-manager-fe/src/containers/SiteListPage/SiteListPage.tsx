50e56c68fca1fdb3747f7984b235a2d10922e54d
Tom Huang <tom6311tom6311@gmail.com>
2022-03-31 23:17:17 +0800

    chore: fill-in user's site info
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- siras-manager-fe/src/containers/SiteListPage/SiteListPage.tsx
+++ siras-manager-fe/src/containers/SiteListPage/SiteListPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useGetSitesQuery } from './SiteListPage.graphql.generated';
 import SiteOverview from '../../components/SiteOverview/SiteOverview';
 
-const SITE_IDS = ['A123456789', 'A223456789'];
+const SITE_IDS = ['BangladeshNo1'];
 
 function SiteListPage() {
   const { loading, error, data } = useGetSitesQuery({ variables: { siteIds: SITE_IDS } });
