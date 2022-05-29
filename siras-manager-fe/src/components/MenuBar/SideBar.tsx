6661c66303522aeb584534b6f00deec159c6937d
Tom Huang <tom6311tom6311@gmail.com>
2022-05-29 22:54:21 +0800

    feat: logout
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- siras-manager-fe/src/components/MenuBar/SideBar.tsx
+++ siras-manager-fe/src/components/MenuBar/SideBar.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import LogoImg from '../../img/logo_h.png';
 
-function SideBar() {
+type SideBarProps = {
+  onLogout: () => void;
+}
+
+function SideBar({ onLogout }: SideBarProps) {
   return (
     <div className="sidebar_wrapper">
       <div className="sidebar_container">
@@ -13,7 +17,8 @@ function SideBar() {
           <div className="sidebar_item_container" style={{ marginTop: '10px' }}><p className="sidebar_item_p">案場總覽</p></div>
         </Link>
         <div className="sidebar_item_container"><p className="sidebar_item_p">GIS圖台</p></div>
-        <div className="sidebar_item_container" style={{ borderWidth: '0' }}><p className="sidebar_item_p">市場資訊</p></div>
+        <div className="sidebar_item_container"><p className="sidebar_item_p">市場資訊</p></div>
+        <button type="button" className="sidebar_item_container" style={{ borderWidth: '0' }} onClick={onLogout}><p className="sidebar_item_p">登出</p></button>
       </div>
     </div>
   );
--- siras-manager-fe/src/containers/MainPage/MainPage.tsx
+++ siras-manager-fe/src/containers/MainPage/MainPage.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useLogoutMutation } from './LogoutMutation.graphql.generated';
 import SideBar from '../../components/MenuBar/SideBar';
 import Header from '../../components/Header/Header';
 
 function MainPage() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+  const [logoutMutation, { data }] = useLogoutMutation();
+
+  // remove the session token when logout succeeded
+  useEffect(() => {
+    if (data?.logout === true) {
+      localStorage.removeItem('x-token');
+      window.location.replace('/login');
+    }
+  }, [data]);
+
   return (
     <div className={isSidebarOpen ? 'sidebar_on' : ''}>
-      <SideBar />
+      <SideBar onLogout={logoutMutation} />
       <Header title="案場總覽" isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <Outlet />
     </div>
